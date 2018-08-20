60fc4e09656e05642db17b7345f180286a7c1495
Ivan <ivanjaratamargo@gmail.com>
2018-08-19 21:39:43 -0300

    subo assets con tsx

---
+++ core/assets/Mapa/redone charset by antifarea 3 sides.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="redone charset by antifarea 3 sides" tilewidth="18" tileheight="20" tilecount="2828" columns="28">
+ <image source="../Characters/redone charset by antifarea 3 sides.png" width="512" height="2024"/>
+</tileset>
---
+++ core/assets/Mapa/sewer_1_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sewer_1_2" tilewidth="16" tileheight="16" tilecount="352" columns="16">
+ <image source="../Texturas de mapa/sewer_1.png" width="256" height="352"/>
+</tileset>
